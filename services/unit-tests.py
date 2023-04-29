67b97544c6d93f3668be172356d828ffc024547d
stefan_pfleger <stefan.pfleger@stud-fh.campuswien.ac.at>
2023-04-29 18:54:19 +0200

    Add Unittests

--- services/unit-tests.py
+++ services/unit-tests.py
@@ -7,6 +7,8 @@ class TestCurrencyConversion(unittest.TestCase):
     def testConversion(self):
         currencyConversion.requestXML()
         self.assertGreater(currencyConversion.convertCurrency("USD", 100, "EUR"), 0)
+        self.assertGreater(currencyConversion.convertCurrency("USD", 100, "JPY"), 0)
+        self.assertEqual(currencyConversion.convertCurrency("USD", 0, "EUR"), 0)
 
 
 unittest.main()
